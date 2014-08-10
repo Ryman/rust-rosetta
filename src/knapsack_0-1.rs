1794edd73f6db2a169361df750c594531278ce08
Kevin Butler <haqkrs@gmail.com>
2014-08-10 23:49:03 +0100

    knapsack 0-1: style

--- src/knapsack_0-1.rs
+++ src/knapsack_0-1.rs
@@ -1,54 +1,58 @@
-extern crate std;
-use std::cmp::max;
-use std::vec::Vec;
+// Implements http://rosettacode.org/wiki/Knapsack_problem/0-1
 
+#![feature(macro_rules)]
+
+use std::cmp::max;
 
 // This struct is used to store our items that we want in our knap-sack.
 //
 // Show is for displaying the fields.
-// allow(dead_code) removes a dead code warning when the name of the struct is not
-// used (as in this case happens in the tests)
-struct Want<'a> {
-    #[allow(dead_code)]
+#[deriving(Show)]
+pub struct Want<'a> {
     name: &'a str,
     weight: uint,
     value: uint
 }
 
+// Global, immutable allocation of our items.
+// This is so we can reference this in multiple functions.
+// We use a macro to generate the array for convenience.
+macro_rules! items(
+    ($($name:expr, $weight:expr, $value:expr),*) => (
+        static ITEMS : &'static [Want<'static>] = &[
+            $( Want { name: $name, weight: $weight, value: $value} ),*
+        ];
+    )
+)
 
-// Global, immutable allocation of our items. This is so we can reference
-// this in multiple functions.
-static items : &'static [Want<'static>] = &[
-    Want {name: "map",                    weight: 9,   value: 150},
-    Want {name: "compass",                weight: 13,  value: 35},
-    Want {name: "water",                  weight: 153, value: 200},
-    Want {name: "sandwich",               weight: 50,  value: 160},
-    Want {name: "glucose",                weight: 15,  value: 60},
-    Want {name: "tin",                    weight: 68,  value: 45},
-    Want {name: "banana",                 weight: 27,  value: 60},
-    Want {name: "apple",                  weight: 39,  value: 40},
-    Want {name: "cheese",                 weight: 23,  value: 30},
-    Want {name: "beer",                   weight: 52,  value: 10},
-    Want {name: "suntancream",            weight: 11,  value: 70},
-    Want {name: "camera",                 weight: 32,  value: 30},
-    Want {name: "T-shirt",                weight: 24,  value: 15},
-    Want {name: "trousers",               weight: 48,  value: 10},
-    Want {name: "umbrella",               weight: 73,  value: 40},
-    Want {name: "waterproof trousers",    weight: 42,  value: 70},
-    Want {name: "waterproof overclothes", weight: 43,  value: 75},
-    Want {name: "note-case",              weight: 22,  value: 80},
-    Want {name: "sunglasses",             weight: 7,   value: 20},
-    Want {name: "towel",                  weight: 18,  value: 12},
-    Want {name: "socks",                  weight: 4,   value: 50},
-    Want {name: "book",                   weight: 30,  value: 10}
-];
+items!("map", 9, 150,
+       "compass", 13, 35,
+       "water", 153, 200,
+       "sandwich", 50, 160,
+       "glucose", 15, 60,
+       "tin", 68, 45,
+       "banana", 27, 60,
+       "apple", 39, 40,
+       "cheese", 23, 30,
+       "beer", 52, 10,
+       "suntancream", 11, 70,
+       "camera", 32, 30,
+       "T-shirt", 24, 15,
+       "trousers", 48, 10,
+       "umbrella", 73, 40,
+       "waterproof trousers", 42, 70,
+       "waterproof overclothes", 43, 75,
+       "note-case", 22, 80,
+       "sunglasses", 7, 20,
+       "towel", 18, 12,
+       "socks", 4, 50,
+       "book", 30, 10)
 
 
 // This is a bottom-up dynamic programming solution to the 0-1 knap-sack problem.
 //      maximize value
 //      subject to weights <= max_weight
-fn knap_01_dp<'a>(xs: &[Want<'a>], max_weight: uint) -> Vec<Want<'a>> {
-
+fn knap_01_dp<'a>(xs: &[Want<'a>], mut capacity: uint) -> Vec<Want<'a>> {
     // Save this value, so we don't have to make repeated calls.
     let xs_len = xs.len();
 
@@ -79,59 +83,50 @@ fn knap_01_dp<'a>(xs: &[Want<'a>], max_weight: uint) -> Vec<Want<'a>> {
     //
     // In a similar vein, the top-down solution would be to memoize the
     // function then compute the results on demand.
-
-    let zero_vec = Vec::from_elem(max_weight + 1, 0 as uint);
-    let mut best_value = Vec::from_elem(xs_len + 1, zero_vec);
+    let zeroes = Vec::from_elem(capacity + 1, 0u);
+    let mut best = Vec::from_elem(xs_len + 1, zeroes);
 
     // loop over the items
     for i in range(0, xs_len) {
         // loop over the weights
-        for w in range(1, max_weight + 1) {
+        for w in range(1, capacity + 1) {
             // do we have room in our knapsack?
-            if xs[i].weight > w {
+            let item = if xs[i].weight > w {
                 // if we don't, then we'll say that the value doesn't change
                 // when considering this item
-                *best_value.get_mut(i + 1).get_mut(w) = best_value[i][w].clone();
+                best[i][w]
             } else {
                 // if we do, then we have to see if the value we gain by adding
                 // the item, given the weight, is better than not adding the item
-                *best_value.get_mut(i + 1).get_mut(w) =
-                    max(best_value[i][w].clone(),
-                        best_value[i][w - xs[i].weight] + xs[i].value);
-            }
+                max(best[i][w], best[i][w - xs[i].weight] + xs[i].value)
+            };
+
+            *best.get_mut(i + 1).get_mut(w) = item;
         }
     }
-
-    // a variable representing the weight left in the bag
-    let mut left_weight = max_weight.clone();
-
-    // a possibly over-allocated dynamically sized vector to push results to
-    let mut result = Vec::with_capacity(xs_len);
 
     // we built up the solution space through a forward pass over the data,
     // now we have to traverse backwards to get the solution
-    for i in range(1, xs_len+1).rev() {
+    range(1, xs_len + 1).rev().fold(Vec::with_capacity(xs_len), |mut result, i| {
         // We can check if an item should be added to the knap-sack by comparing
         // best_value with and without this item. If best_value added this
         // item then so should we.
-        if best_value[i][left_weight] != best_value[i - 1][left_weight] {
+        if best[i][capacity] != best[i - 1][capacity] {
             result.push(xs[i - 1]);
-            // we remove the weight of the object from the remaining weight
-            // we can add to the bag
-            left_weight -= xs[i - 1].weight;
+            // we remove the weight of the object from the bag's remaining capacity
+            capacity -= xs[i - 1].weight;
         }
-    }
 
-    return result;
+        result
+    })
 }
-
 
 #[cfg(not(test))]
 fn main () {
-    let xs = knap_01_dp(items, 400);
+    let xs = knap_01_dp(ITEMS, 400);
 
-    // Print the items. We have to reverse the order because we solved the
-    // problem backward.
+    // Print the items.
+    // We have to reverse the order because we solved the problem backwards.
     for i in xs.iter().rev() {
         println!("Item: {}, Weight: {}, Value: {}", i.name, i.weight, i.value);
     }
@@ -143,14 +138,12 @@ fn main () {
     // Print the sum of the values.
     let values = xs.iter().fold(0, |a, &b| a + b.value);
     println!("Total Value: {}", values);
-
 }
-
 
 #[test]
 fn test_dp_results() {
-    let dp_results = knap_01_dp(items, 400);
-    let dp_weights= dp_results.iter().fold(0, |a, &b| a + b.weight);
+    let dp_results = knap_01_dp(ITEMS, 400);
+    let dp_weights = dp_results.iter().fold(0, |a, &b| a + b.weight);
     let dp_values = dp_results.iter().fold(0, |a, &b| a + b.value);
     assert_eq!(dp_weights, 396);
     assert_eq!(dp_values, 1030);
