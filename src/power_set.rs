764d2620d459fa1edcf13277be9703c4221f162b
Kevin Butler <haqkrs@gmail.com>
2014-08-11 23:01:36 +0100

    power set: reduce clones + style

--- src/power_set.rs
+++ src/power_set.rs
@@ -1,50 +1,38 @@
 // Given a set, generate its power set, which is the set of all subsets of that
 // set: http://rosettacode.org/wiki/Power_set
 
-use std::vec::Vec;
-use std::slice::Items;
-
 // If set == {}
 //   return {{}}
 // else if set == {a} U rest
 //   return power_set(rest) U ({a} U each set in power_set(rest))
-fn power_set<'a, T: Clone>(items: &mut Items<'a,T>) -> Vec<Vec<T>> {
-    let mut power = Vec::new();
+fn power_set<T: Clone, I: Iterator<T>>(mut items: I) -> Vec<Vec<T>> {
     match items.next() {
-        None       => power.push(Vec::new()),
+        None => vec![vec![]],
         Some(item) => {
-            for set in power_set(items).iter() {
-                power.push(set.clone());
-                power.push(set.clone().append_one(item.clone()));
-            }
+            power_set(items).move_iter().fold(vec![], |power, set| {
+                power.append_one(set.clone())
+                     .append_one(set.append_one(item.clone()))
+            })
         }
     }
-    power
+}
+
+#[cfg(not(test))]
+fn main() {
+    let set = &[1i, 2, 3, 4];
+    let power = power_set(set.iter());
+    println!("Set      : {}", set);
+    println!("Power Set: {}", power);
 }
 
 #[test]
 fn test() {
     let set = Vec::<int>::new();
-    let power = power_set(&mut set.iter());
-    assert!(power == vec!(vec!()));
+    let power = power_set(set.iter());
+    assert_eq!(power, vec![vec![]]);
 
-    let mut set = Vec::<int>::new();
-    set.push(1);
-    set.push(2);
-    set.push(3);
-    let power = power_set(&mut set.iter());
-    assert!(power == vec!(vec!(), vec!(1), vec!(2), vec!(2, 1),
-                          vec!(3), vec!(3, 1), vec!(3, 2), vec!(3, 2, 1)));
-}
-
-#[cfg(not(test))]
-fn main() {
-    let mut set = Vec::<int>::new();
-    set.push(1);
-    set.push(2);
-    set.push(3);
-    set.push(4);
-    let power = power_set(&mut set.iter());
-    println!("Set      : {}", set);
-    println!("Power Set: {}", power);
+    let set = &[1i, 2, 3];
+    let power = power_set(set.iter().map(|&i| i));
+    assert_eq!(power, vec![vec![], vec![1], vec![2], vec![2, 1],
+                          vec![3], vec![3, 1], vec![3, 2], vec![3, 2, 1]]);
 }
