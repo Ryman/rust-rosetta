c1af04fb816c4bfd43ddcc7a71105cef470f145a
Kevin Butler <haqkrs@gmail.com>
2014-08-13 22:08:09 +0100

    range expansion: reduce allocations + style

--- src/range_expansion.rs
+++ src/range_expansion.rs
@@ -18,17 +18,17 @@ fn expand_range(range: &str) -> Vec<int> {
     let mut result = vec![];
 
     for item in range.split(',') {
-        result.extend(expand_item(item).move_iter());
+        expand_item(item, &mut result)
     }
 
     result
 }
 
 // Expand a single element, which can be a number or a range.
-fn expand_item(item: &str) -> Vec<int> {
+fn expand_item(item: &str, result: &mut Vec<int>) {
     // Handle the case of a single number
     for cap in regex!(r"^(-?\d+)$").captures_iter(item) {
-        return vec![from_str(cap.at(0)).unwrap()]
+        return result.push(from_str(cap.at(0)).unwrap());
     }
 
     // Handle the case of a range
@@ -37,7 +37,7 @@ fn expand_item(item: &str) -> Vec<int> {
         let right = from_str(cap.at(2)).unwrap();
 
         // Generate and collect a range between them
-        return range_inclusive(left, right).collect()
+        return result.extend(range_inclusive(left, right));
     }
 
     fail!("The item `{}` is not a number or a range!", item);
@@ -46,16 +46,17 @@ fn expand_item(item: &str) -> Vec<int> {
 #[test]
 fn test_basic() {
     let range = "1-5,6";
-    assert!(expand_range(range).as_slice() == [1, 2, 3, 4, 5, 6]);
+    assert_eq!(expand_range(range).as_slice(), &[1, 2, 3, 4, 5, 6]);
 
     let range = "-6,-3-1,3-5,7-11,14,15,17-20";
-    assert!(expand_range(range).as_slice() ==
-        [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+    assert_eq!(expand_range(range).as_slice(),
+               &[-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8,
+                 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 }
 
 #[test]
 #[should_fail]
 fn test_wrong() {
     let range = "one-five,six";
-    assert!(expand_range(range).as_slice() == [1, 2, 3, 4, 5, 6]);
+    assert_eq!(expand_range(range).as_slice(), &[1, 2, 3, 4, 5, 6]);
 }
